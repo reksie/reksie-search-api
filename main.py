e7b5fb7b22d60b900280c6051beac6c69e01e723
Wes Cole <wesley.cole87@gmail.com>
2024-05-09 18:43:46 -0400

    .

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import unkey
+
+from dotenv import load_dotenv
+from typing import Annotated, Any, Dict, Optional
+from fastapi import Body, FastAPI, Header
+from pydantic import BaseModel, Field
+from pinecone_text.sparse import BM25Encoder 
+
+load_dotenv()
+
+app = FastAPI()
+bm25 = BM25Encoder().default()
+
+def key_extractor(*args: Any, **kwargs: Any) -> Optional[str]:
+    if isinstance(auth := kwargs.get("authorization"), str):
+        return auth.split(" ")[-1]
+
+    return None
+
+class Document(BaseModel):
+    content: str | None = Field(
+        default=None, title="Content to embed"
+    )
+
+@app.get("/")
+async def read_root():
+    return {"status": "Okay!"}
+
+@app.post("/embeddings")
+@unkey.protected(os.environ["UNKEY_API_ID"], key_extractor)
+async def protected_route(
+    document: Annotated[Document, Body(embed=True)],
+    authorization: str = Header(None),
+    unkey_verification: Any = None,
+):
+    assert isinstance(unkey_verification, unkey.ApiKeyVerification)
+    assert unkey_verification.valid
+
+    doc_sparse_vector = bm25.encode_documents(document.content)
+
+    return doc_sparse_vector
