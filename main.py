2505b34e6242e4694b71b6bd61314591cb323c30
Wes Cole <wesley.cole87@gmail.com>
2024-05-09 22:32:41 -0400

    .

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import unkey
 from dotenv import load_dotenv
 from typing import Annotated, Any, Dict, Optional
 from fastapi import Body, FastAPI, Header
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from pinecone_text.sparse import BM25Encoder 
 
@@ -11,6 +12,18 @@ load_dotenv()
 
 app = FastAPI()
 bm25 = BM25Encoder().default()
+
+origins = [
+  '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def key_extractor(*args: Any, **kwargs: Any) -> Optional[str]:
     if isinstance(auth := kwargs.get("authorization"), str):
@@ -26,6 +39,10 @@ class Document(BaseModel):
 @app.get("/")
 async def read_root():
     return {"status": "Okay!"}
+
+@app.get("/healthcheck")
+def read_root():
+    return {"status": "ok"}
 
 @app.post("/embeddings")
 @unkey.protected(os.environ["UNKEY_API_ID"], key_extractor)
