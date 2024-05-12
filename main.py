87e6e152cd5aff79af9f9998af60a960e2712ecd
Wes Cole <wesley.cole87@gmail.com>
2024-05-12 00:39:14 -0400

    .

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import os
 import unkey
 
 from dotenv import load_dotenv
-from typing import Annotated, Any, Dict, Optional
+from typing import Annotated, Any, Optional, Union, List
 from fastapi import Body, FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from pinecone_text.sparse import BM25Encoder 
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
 app = FastAPI()
 bm25 = BM25Encoder().default()
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 origins = [
   '*'
@@ -32,13 +34,12 @@ def key_extractor(*args: Any, **kwargs: Any) -> Optional[str]:
     return None
 
 class Document(BaseModel):
-    content: str | None = Field(
+    chunks: List[str] | None = Field(
         default=None, title="Content to embed"
     )
-
-@app.get("/")
-async def read_root():
-    return {"status": "Okay!"}
+    sparse_content: Union[str | None] = Field(
+        default=None, title="Sparse Content to embed"
+    )
 
 @app.get("/healthcheck")
 def read_root():
@@ -46,7 +47,7 @@ def read_root():
 
 @app.post("/embeddings")
 @unkey.protected(os.environ["UNKEY_API_ID"], key_extractor)
-async def protected_route(
+async def embeddings(
     document: Annotated[Document, Body(embed=True)],
     authorization: str = Header(None),
     unkey_verification: Any = None,
@@ -54,6 +55,11 @@ async def protected_route(
     assert isinstance(unkey_verification, unkey.ApiKeyVerification)
     assert unkey_verification.valid
 
-    doc_sparse_vector = bm25.encode_documents(document.content)
+    vectors = model.encode(document.chunks)
 
-    return doc_sparse_vector
+    if document.sparse_content is not None:
+        doc_sparse_vector = bm25.encode_documents([document.sparse_content])
+    else:
+        doc_sparse_vector = None
+
+    return {"vectors": vectors.tolist(), "sparse_vector": doc_sparse_vector} 
