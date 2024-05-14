24f1efbef18b796c0998dd0f0208c713f7100c4e
Wes Cole <wesley.cole87@gmail.com>
2024-05-13 23:54:05 -0400

    get api working and add completion endpoint

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,15 +1,20 @@
 import os
 import unkey
 
+from dotenv import load_dotenv
 from pydantic import BaseModel, root_validator
 from typing import  Any, Dict, Optional
 from fastapi import APIRouter, Header
+from fastapi.responses import StreamingResponse
 from pinecone import Pinecone
+from openai import OpenAI
 from ..dependencies import create_embeddings, split_text
+
+load_dotenv()
 
 router = APIRouter()
 pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
-
+openai = OpenAI()
 
 class Match(BaseModel):
     id: str
@@ -17,16 +22,9 @@ class Match(BaseModel):
     metadata: Optional[Dict[str, Any]]
 
 
-class Metadata(BaseModel):
-    id: str
-
-    class Config:
-        extra = "allow"
-
-
 class UpsertInput(BaseModel):
     content: str
-    metadata: Optional[Metadata]
+    metadata: Optional[Dict[str, Any]]
 
 
 class DeleteInput(BaseModel):
@@ -49,6 +47,10 @@ class QueryInput(BaseModel):
     alpha: Optional[float] = None
     metadata: Optional[Dict[str, Any]] = None
 
+class PromptInput(BaseModel):
+    query: str
+    stream: Optional[bool] = False
+
 
 def key_extractor(*args: Any, **kwargs: Any) -> Optional[str]:
     if isinstance(auth := kwargs.get("authorization"), str):
@@ -56,10 +58,15 @@ def key_extractor(*args: Any, **kwargs: Any) -> Optional[str]:
 
     return None
 
+def generate_stream(response):
+    for event in response:
+        if hasattr(event.choices[0].delta, 'content'):
+            current_response = event.choices[0].delta.content
+            yield f"data: {current_response}\n\n"
 
 @router.post("/upsert", tags=["documents"])
 # @unkey.protected(os.environ["UNKEY_API_ID"], key_extractor)
-async def read_documents(
+async def upsert_documents(
     input: UpsertInput,
     authorization: str = Header(None),
     unkey_verification: Any = None,
@@ -71,7 +78,7 @@ async def read_documents(
     index = pc.Index("reksie-search-dev")
 
     chunks = await split_text(input.content)
-    metadata_dict = input.metadata.dict() if input.metadata else {}
+    metadata_dict = input.metadata if input.metadata else {}
 
     vectors = []
 
@@ -82,7 +89,10 @@ async def read_documents(
                 "id": f"{owner_id}#{metadata_dict.get('id')}#chunk_{chunk_index}",
                 "values": dense_vector,
                 "sparse_values": sparse_vector,
-                "metadata": metadata_dict,
+                "metadata": {
+                    **metadata_dict,
+                    "content": chunk.page_content,
+                },
             }
         )
 
@@ -144,3 +154,42 @@ async def query_documents(
     ]
 
     return {"matches": matches}
+
+@router.post("/completion", tags=["documents"])
+# @unkey.protected(os.environ["UNKEY_API_ID"], key_extractor)
+async def prompt_completion(
+    input: PromptInput, authorization: str = Header(None), unkey_verification: Any = None
+):
+    # assert isinstance(unkey_verification, unkey.ApiKeyVerification)
+    # assert unkey_verification.valid
+    # owner_id = unkey_verification.owner_id
+    owner_id = "user_2f4XwosSCKSJ7sS99MOXEayr47q"
+    index = pc.Index("reksie-search-dev")
+
+    dense_vector, sparse_vector = await create_embeddings(
+        query=input.query, alpha=0.8
+    )
+
+    results = index.query(
+        namespace=owner_id,
+        vector=dense_vector,
+        sparse_vector=sparse_vector,
+        top_k=3,
+        include_metadata=True,
+    )
+
+    matches = [
+        Match(id=match["id"], score=match["score"], metadata=match.get("metadata"))
+        for match in results.get("matches", [])
+    ]
+
+    completion = openai.chat.completions.create(model="gpt-4o", messages=[
+        {"role": "system", "content": "Use the following content to answer the prompt"},
+        {"role": "system", "content": " ".join([match.metadata["content"] for match in matches if match.metadata and "content" in match.metadata])},
+        {"role": "user", "content": input.query}
+    ], stream=input.stream)
+
+    if input.stream:
+        return StreamingResponse(generate_stream(completion), media_type="text/event-stream")
+
+    return {"completion": completion.choices[0].message.content}
